fdfaf9872d4581146b60b1f8b241930cda99018a
Cloves Paiva <cloves.paiva@extremedigital.com.br>
2023-02-23 08:33:31 -0300

    details and realted itens

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -6,4 +6,5 @@ from .models import Item
 # Create your views here.
 def detail(request, primary_key):
     item = get_object_or_404(Item, pk=primary_key)
-    return render(request, "item/detail.html", {"item": item})
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=primary_key)
+    return render(request, "item/detail.html", {"item": item, "related_items": related_items})
